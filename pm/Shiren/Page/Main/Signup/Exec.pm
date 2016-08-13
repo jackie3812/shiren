b5f48e0e0cf8a595eb63387b4ebbf9c4de8e27d1
jackie kim <jk1991kim@gmail.com>
2016-08-13 17:55:28 +0900

    use SHA

--- pm/Shiren/Page/Main/Signup/Exec.pm
+++ pm/Shiren/Page/Main/Signup/Exec.pm
@@ -3,7 +3,7 @@ package Shiren::Page::Main::Signup::Exec;
 use strict;
 use warnings;
 
-use Digest::SHA;
+use Digest::SHA qw/sha256/;
 
 use Shiren::Func::User;
 use Shiren::Func::Util qw/redirect_to global_time/;
@@ -12,9 +12,9 @@ use parent qw/Shiren::Page::Base/;
 sub validate_conditions {
 	my $class = shift;
 	return +[
-		name      => ['NOT_BLANK', 'ASCII', ['LENGTH', 1, 20]],
-		password  => ['NOT_BLANK', 'ASCII', ['LENGTH', 1, 20]],
-		password2 => ['NOT_BLANK', 'ASCII', ['LENGTH', 1, 20]]
+		name  => ['NOT_BLANK', 'ASCII', ['LENGTH', 1, 20]],
+		pass  => ['NOT_BLANK', 'ASCII', ['LENGTH', 1, 20]],
+		pass2 => ['NOT_BLANK', 'ASCII', ['LENGTH', 1, 20]]
 	];
 }
 
@@ -25,6 +25,11 @@ sub pre_action {
 
 	# すでに登録済みのnameはダメ
 	return redirect_to($c, "/signup/index") if Shiren::Func::User->is_registered_name($c, $req->param("name"));
+
+	# passが一致しているかの確認
+	my $digested_pass = sha256($req->param("pass"));
+	my $digested_pass2 = sha256($req->param("pass2"));
+	return redirect_to($c, "/signup/index") unless $digested_pass eq $digested_pass2;
 }
 
 
